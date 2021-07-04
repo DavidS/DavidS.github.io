2f718c2c1c4b1cd9daa00bac2e1ea49d04cf0745
David Schmitt <david@black.co.at>
2021-07-04 14:58:52 +0100

    add auto gem install

--- bin/build.sh
+++ bin/build.sh
@@ -4,6 +4,7 @@ set -e
 
 cd /srv/davidlog/git/
 
+bundle check || bundle install
 git annex lock .
 git commit -m "lock server" || true # if no changes
 JEKYLL_ENV=production bundle exec jekyll build --strict --verbose --destination /srv/davidlog/site
